b769a3921aaacd33cbebb0e013f93bb69ccd42a7
Sunil <sunillangtad54@gmail.com>
2019-05-15 16:59:11 +0530

    There can be multiple classes in a java file

--- Basics/ClassName.java
+++ Basics/ClassName.java
@@ -6,3 +6,12 @@ class A {
   System.out.println("This is something that is not from the book");
  }
 }
+
+/**
+* One java file can have multiple classes in it
+*/
+class B {
+ void display() {
+  System.out.println("This is from class B");
+ }
+}
