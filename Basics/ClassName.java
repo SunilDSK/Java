b0215ede5b5450ab0cabd5de8c9853c084aceda5
Sunil <sunillangtad54@gmail.com>
2019-05-15 16:49:11 +0530

    File name and class name need not be same

---
+++ Basics/ClassName.java
@@ -0,0 +1,8 @@
+/**
+* class A to demonstrate that the file name and class name need not be same
+*/
+class A {
+ public static void main(String[] args) {
+  System.out.println("This is something that is not from the book");
+ }
+}
