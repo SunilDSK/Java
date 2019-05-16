6d83484465a5ea781258ce19105526328bf33317
Sunil <sunillangtad54@gmail.com>
2019-05-16 16:30:28 +0530

    Demo classes for package

---
+++ packages/ClassTwo.java
@@ -0,0 +1,9 @@
+/**
+* This a demo class ClassTwo to demonstrate packaging
+*/
+package packageOne; // Adds itself to packageOne
+public class ClassTwo {
+	public void display() {
+		System.out.println("This is from ClassTwo");
+	}
+}
