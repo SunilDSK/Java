c29feed3b9d365cd61f99515f05c8fd3b1acbfad
Sunil <sunillangtad54@gmail.com>
2019-05-25 13:33:36 +0530

    Public access specifier

---
+++ Access specifiers/public/driver.java
@@ -0,0 +1,13 @@
+/**
+* This is a driver program. It uses classes from pachageOne.
+*/
+package packageTwo; // Adds itself to packageOne
+import packageOne.*; // Imports all the classes from packageOne
+public class driver {
+	public static void main(String[] args) {
+		ClassOne one = new ClassOne();
+		ClassTwo two = new ClassTwo();
+		one.display();
+		two.display();
+	}
+}
